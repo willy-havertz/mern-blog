68e3b5fb7565e0e333ca260cb7786de32f817381
willy-havertz <wiltordichingwa@gmail.com>
2025-07-18 04:21:57 +0300

    client and server remodels

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,15 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import AuthProvider from "./context/AuthProvider";
-import "./index.css"; 
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
-
-Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_FRONTEND_DSN,
-  integrations: [new BrowserTracing()],
-  tracesSampleRate: 1.0,
-});
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
